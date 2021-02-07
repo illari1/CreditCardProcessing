a98f81cbee27761624e19dc13a38a8c827222b61
raquela <raquela@spacebarmedia.com>
2021-02-07 14:07:35 +0000

    renamed The entry point of the Spring Boot Application

--- src/main/java/com/publicis/microservices/creditcards/service/CardService.java
+++ src/main/java/com/publicis/microservices/creditcards/service/CardService.java
@@ -22,6 +22,9 @@ public class CardService {
                 .name(registrationCardRequest.getName())
                 .build();
 
-        return  cardRepository.save(creditCard);
+        return cardRepository.save(creditCard);
     }
 }
+
+
+
